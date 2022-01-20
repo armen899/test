8d39b1fa80e8b63b8ae8f8953ff2b956c43bb327
Armen Margaryan <armenmargaryan89@gmail.com>
2022-01-20 13:59:32 +0400

    Updates

---
+++ precache-manifest.ccb81aff7dc90c52f0f997f706c0d6c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40609f3fff3e5d718d44d9bd12a2a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d108dd9d6562dad3f3",
+    "url": "/static/css/2.ab3e4624.chunk.css"
+  },
+  {
+    "revision": "5cde5e0898b12aa8cee2",
+    "url": "/static/css/main.26e019cc.chunk.css"
+  },
+  {
+    "revision": "22d108dd9d6562dad3f3",
+    "url": "/static/js/2.6d755436.chunk.js"
+  },
+  {
+    "revision": "d4dcc7030a52eef6748455ea4c9ae726",
+    "url": "/static/js/2.6d755436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cde5e0898b12aa8cee2",
+    "url": "/static/js/main.d52e5b46.chunk.js"
+  },
+  {
+    "revision": "6e48746dc5e711a3e43e",
+    "url": "/static/js/runtime-main.40eca786.js"
+  },
+  {
+    "revision": "b571779073e7de6796ed1d4887e740e4",
+    "url": "/static/media/err.b5717790.jpg"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  }
+]);
