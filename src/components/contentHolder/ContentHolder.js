13424d00136eebe1807315977b44d008db4b27e4
Armen Margaryan <armenmargaryan89@gmail.com>
2022-01-20 13:41:43 +0400

    Create gh-pages branch with static content

--- src/components/contentHolder/ContentHolder.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ContentHolder.less";
-
-const ContentHolder = (props) => {
-  return <div className='contentHolder'>{props.children}</div>;
-};
-
-export default ContentHolder;
--- src/components/noData/NoData.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Empty } from 'antd'
-
-const NoData = () => {
-  return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-}
-
-export default NoData
