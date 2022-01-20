10c62c92597410d687ad0c95063ec5569cf5cf2e
Armen Margaryan <armenmargaryan89@gmail.com>
2022-01-20 15:31:10 +0400

    Updates

---
+++ precache-manifest.b952f5e71e86d670e85b748043dcc60d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfa72b58bc57d7a65c16fb922895f12",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ffa46dc0b649fc16a8f8",
+    "url": "/test/static/css/2.ab3e4624.chunk.css"
+  },
+  {
+    "revision": "620ad8fdd936cf510413",
+    "url": "/test/static/css/main.26e019cc.chunk.css"
+  },
+  {
+    "revision": "ffa46dc0b649fc16a8f8",
+    "url": "/test/static/js/2.ec3a249c.chunk.js"
+  },
+  {
+    "revision": "d4dcc7030a52eef6748455ea4c9ae726",
+    "url": "/test/static/js/2.ec3a249c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620ad8fdd936cf510413",
+    "url": "/test/static/js/main.9df507c2.chunk.js"
+  },
+  {
+    "revision": "575230ed533b7476e94f",
+    "url": "/test/static/js/runtime-main.b133f454.js"
+  },
+  {
+    "revision": "b571779073e7de6796ed1d4887e740e4",
+    "url": "/test/static/media/err.b5717790.jpg"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/test/static/media/getFetch.51c934af.cjs"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.917d41a88903d55786c90744dd1fb230.js"
+  "/test/precache-manifest.b952f5e71e86d670e85b748043dcc60d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
